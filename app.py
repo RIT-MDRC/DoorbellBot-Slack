becb3445c158265454a869e2ab5426cd10b98ac9
Cameron Robinson <cvr8924@rit.edu>
2022-10-10 18:03:11 -0400

    Undocumented mvp
    ~CR

--- app.py
+++ app.py
@@ -5,26 +5,23 @@ from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 import requests
 
-
 # Install the Slack app and get xoxb- token in advance
 app = App(token=os.environ["SLACK_BOT_TOKEN"])
 
-@app.command("/hello-socket-mode")
-def hello_command(ack, body):
-    user_id = body["user_id"]
-    ack(f"Hi, <@{user_id}>!")
 
-@app.event("app_mention")
-def event_test(say):
-    print("Hi there!")
+@app.command("/door")
+def door_command(ack, body):
+    response = requests.get('http://129.21.121.208:8080/door')
+    user_id = body["user_id"]
+    ack(f"{response.text}, <@{user_id}>!")
+
+
+@app.command("/elevator")
+def elevator_command(ack, body):
+    response = requests.get('http://129.21.121.208:8080/elevator')
+    user_id = body["user_id"]
+    ack(f"{response.text}, <@{user_id}>!")
+
 
 if __name__ == "__main__":
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
-
-
-
-
-    #print("test")
-    #asyncio.run(doShit())
-
-
