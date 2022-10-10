58014fd138a7a87720bfd6bced5847a60ba59b6e
Cameron Robinson <cvr8924@rit.edu>
2022-10-10 19:09:30 -0400

    Version 1.0.0
    ~CR

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-import asyncio
-import os
+# Author: Cameron Robinson
+# Email: cvr8924@rit.edu
+# Date: 10/10/22
+# Slack App for calling doorbell bot
 
+import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 import requests
@@ -10,11 +13,13 @@ app = App(token=os.environ["SLACK_BOT_TOKEN"])
 
 
 @app.command("/door")
-def door_command(ack, body, client):
+def door_command(ack, body):
     try:
         response = requests.get(f'http://{os.environ["IP_ADDRESS"]}/door')
         out = response.text
-    except:
+    except: # I want this to handle a broad range of errors ~CR
+        out = "Oops! Something went wrong"
+    if response.status_code != 200:
         out = "Oops! Something went wrong"
     user_id = body["user_id"]
     ack(f"{out}, <@{user_id}>!")
@@ -27,9 +32,12 @@ def elevator_command(ack, body):
         out = response.text
     except:
         out = "Oops! Something went wrong"
+    if response.status_code != 200:
+        out = "Oops! Something went wrong"
     user_id = body["user_id"]
     ack(f"{out}, <@{user_id}>!")
 
 
+# Start Websocket Relationship with slack
 if __name__ == "__main__":
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
